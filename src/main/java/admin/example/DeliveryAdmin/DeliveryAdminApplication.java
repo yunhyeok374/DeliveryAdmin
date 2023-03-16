5b174ac366ba088ed6cf6eeb8eac000e933b1423
YunHyeok <djbyh645047@naver.com>
2023-03-16 22:23:55 +0900

    Initial commit

---
+++ src/main/java/admin/example/DeliveryAdmin/DeliveryAdminApplication.java
@@ -0,0 +1,13 @@
+package admin.example.DeliveryAdmin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeliveryAdminApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DeliveryAdminApplication.class, args);
+	}
+
+}
---
+++ src/test/java/admin/example/DeliveryAdmin/DeliveryAdminApplicationTests.java
@@ -0,0 +1,13 @@
+package admin.example.DeliveryAdmin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeliveryAdminApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
